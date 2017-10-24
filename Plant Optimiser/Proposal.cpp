a0ce8a81eab7f7b1061566337d5149c37724d405
Foxh0und <amiritis@wolfmail.co>
2017-10-25 09:51:17 +1100

    Update Proposal.cpp

--- Plant Optimiser/Proposal.cpp
+++ Plant Optimiser/Proposal.cpp
@@ -1,20 +1,12 @@
-//
-//  Proposal.cpp
-//  Plant
-//
-//  Created by Adam Miritis on 13/10/2015.
-//  Copyright © 2015 Adam Miritis. All rights reserved.
-//
-
 #include "Proposal.h"
 
-
-Proposal::Proposal( int aIndex,int aCost, int aRevenue )
+Proposal::Proposal( int aIndex, int aCost, int aRevenue )
 {
     fIndex = aIndex;
     fCost = aCost;
     fRevenue = aRevenue;
 }
+
 int Proposal::getIndex() const
 {
     return fIndex;
